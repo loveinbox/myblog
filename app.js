2655aa5ab9ab8a7f6b7f1f9eaa0900ad52d3cd2c
Hank Han <dhuhank@foxmail.com>
2015-12-27 09:38:17 +0800

    add time in ip list

--- app.js
+++ app.js
@@ -27,38 +27,42 @@ app.get('/', function(req, res, next) {
             var db = new sqlite3.Database(__dirname + '/public/' + 'mydb.db');
             db.serialize(function() {
 
-                db.run("CREATE TABLE if not exists request_info (id integer PRIMARY KEY autoincrement, ip TEXT, remoteAddress TEXT, headers TEXT)");
-                var stmt = db.prepare("INSERT INTO request_info (ip, remoteAddress, headers) VALUES (?,?,?)");
-                stmt.run(util.inspect(getClientAddress(req)), util.inspect(req.connection.remoteAddress), util.inspect(req.headers));
+                db.run("CREATE TABLE if not exists request_time (time TEXT PRIMARY KEY, ip TEXT, remoteAddress TEXT, headers TEXT)");
+                var stmt = db.prepare("INSERT INTO request_time VALUES (?,?,?,?)");
+                stmt.run(util.inspect(Date()), util.inspect(getClientAddress(req)), util.inspect(req.connection.remoteAddress), util.inspect(req.headers));
                 stmt.finalize();
                 console.log('insert');
             });
             db.close();
         }
     });
-    var getClientAddress = function (req) {
-        return (req.headers['x-forwarded-for'] || '').split(',')[0] 
-            || req.connection.remoteAddress;
+    var getClientAddress = function(req) {
+        return (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress;
     };
 });
 
 app.get('/db', function(req, res, next) {
     var stat = fs.statSync(dbname);
-    res.writeHeader(200,{"Content-Length":stat.size,'Content-disposition':'attachment; filename=' + 'mydb.db'});
+    res.writeHeader(200, {
+        "Content-Length": stat.size,
+        'Content-disposition': 'attachment; filename=' + 'mydb.db'
+    });
     var fReadStream = fs.createReadStream(dbname);
     fReadStream.pipe(res);
 });
 
 app.get('/ipData', function(req, res, next) {
-    var ipPac = {'rows': []};
+    var ipPac = {
+        'rows': []
+    };
     var sqlite3 = require('sqlite3').verbose();
     var db = new sqlite3.Database(__dirname + '/public/' + 'mydb.db');
     // res.setHeader('Content-Type', 'application/json');
     db.serialize(function() {
-        db.each("SELECT * FROM request_info", function(err, row) {
-            ipPac.rows.unshift(row);           
-        }, function (){
-                console.log(ipPac);
+        db.each("SELECT * FROM request_time", function(err, row) {
+            ipPac.rows.unshift(row);
+        }, function() {
+            // console.log(ipPac);
             res.write(JSON.stringify(ipPac));
             res.end();
         });
@@ -68,7 +72,7 @@ app.get('/ipData', function(req, res, next) {
 
 app.use(express.static('public'));
 
-var server = app.listen(80, function() {
+var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
