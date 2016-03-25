7b84269d3d713ad83975bf74eba5554825a3e33b
Hank Han <dhuhank@foxmail.com>
2016-03-25 08:34:20 +0800

    node change test 03

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.all('/gitpull', function(req, res, next) {
 });
 
 app.all('/version', function(req, res, next) {
-    res.send('02version:' + util.inspect(version));
+    res.send('03version:' + util.inspect(version));
 });
 
 function getTime() {
