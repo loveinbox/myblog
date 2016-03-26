c9d417fd2d0d21e499c013f36de20a079659a436
Hank Han <dhuhank@foxmail.com>
2016-03-27 00:10:55 +0800

    test watch folder,just for .js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 var version = getTime();
 
 app.use(express.static('public'));
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
