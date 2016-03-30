ceba6b8d8d206ac142b7b062ab71ef4283e3b1e2
Hank Han <dhuhank@foxmail.com>
2016-03-30 19:27:58 +0800

    scroll to show/hide header

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,4 +1,4 @@
-;(function getVersion () {
+$(function getVersion () {
     $("<header>").attr('id', 'main-header')
         .load( "pages/header.html", function () {
             $.ajax({
@@ -8,4 +8,32 @@
             });
         })
         .prependTo('body');
-})();
+
+    var isScrollingDown = true,
+        tempHeight = 0,
+        gap = 0,
+        height = 30;
+    var header = $("#main-header");
+
+    $(window).scroll(function(event) {
+        console.log($('body').scrollTop());
+        gap = tempHeight - $('body').scrollTop();
+        console.log('gap',gap);
+        console.log('isScrollingDown',isScrollingDown);
+        if(gap < 0){
+            height = 0;
+        }else{
+            height = 52;
+        }
+        if(gap < 0){
+            isScrollingDown = true
+        }
+        // height += gap;
+        // if(height > 30){
+        //     height = 30;
+        // }
+        header.height(height); 
+        // console.log('height', height);
+        tempHeight = $('body').scrollTop();
+    });;
+});
